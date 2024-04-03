b648b6ca00c6dd1d206d8174c2f28191c1a78fa5
Aaron Randolph <arandolph@bakerripley.org>
2024-04-03 10:55:47 -0500

    Change entityNames to IEnummerable

--- Classes/DTOs/EntityDto.cs
+++ Classes/DTOs/EntityDto.cs
@@ -8,6 +8,6 @@ namespace ChromeRiverService.Classes.DTOs
         public string? EntityCode { get; set; }
         public string? EntityTypeCode { get; set; }
         public string? ExtraData1 { get; set; }
-        public ICollection<EntityName>? EntityNames { get; set; }
+        public IEnumerable<EntityName>? EntityNames { get; set; }
     }
 }
